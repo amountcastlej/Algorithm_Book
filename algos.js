a0cbfcd34d42a86b99d821e65b07edabdb47a309
Adam Joines <amountcastlej@gmail.com>
2021-10-14 20:53:43 -0400

    "New JS algos added"

--- algos.js
+++ algos.js
@@ -488,3 +488,16 @@ function letterGrade(score){
 }
 
 letterGrade(62)
+
+function shuffle(arr){
+    var idx;
+    var temp;
+    for (var i = 0; i < arr.length; i++){
+        idx = Math.trunc(Math.random()*arr.length)
+        temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+    }
+    return arr;
+}
+shuffle([1,2,3,4,5,6,7,8,9,10])
